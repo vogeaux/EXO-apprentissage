e12ac844e1f0ac9196d32146121c71330d231392
kevin <kevin.hamon@campus-igs-toulouse.fr>
2022-02-28 17:39:37 +0100

    TP 1 PARTI 1

--- EXO-graven/TP1/TP1.py
+++ EXO-graven/TP1/TP1.py
@@ -1,25 +1,54 @@
-from asyncio.windows_events import NULL
 import random
 
+def niveau1():
 
-bot_be_ja = random.randrange(1,5)
-print (bot_be_ja)
+    bot_be_ja = random.randrange(1,5)
+    print (bot_be_ja)
 
-while True:
-    try:
-        j_nbr=int(input("chosir la jarre sur les 5 jarres: "))
-    except:
-        print("valeur incorrecte")
-        continue
-    else:
-        break
-        
-        
-if j_nbr == bot_be_ja:
-    print("Piege")
-else :
-    print("Gagné")
+    while True:
+        try:
+            j_nbr=int(input("chosir la jarre sur les 5 jarres: "))
+        except:
+            print("valeur incorrecte")
+            continue
+        else:
+            break
+            
+            
+    if j_nbr == bot_be_ja:
+        print("Piege")
+    else :
+        print("Gagné")
 
 
+def niveau2():
+
+    trousseau = 0
+    bot_be_ja = random.randrange(1,5)
+    print (bot_be_ja)
+
+    while True:
+        while True:
+            try:
+                j_nbr=int(input("chosir la jarre sur les 5 jarres: "))
+            except:
+                print("valeur incorrecte")
+                continue
+            else:
+                break
+                
+                
+        if j_nbr == bot_be_ja:
+            print("Piege")
+            trousseau=trousseau-1
+            if trousseau < 0:
+                print("partie perdu")
+        else :
+            print("Gagné")
+            trousseau=trousseau+1
+            if trousseau > 2:
+                print("Tu deviens roi du temple")
+
+niveau2()
 
 
