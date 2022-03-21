08338a76db34d128f7e8186975b0e353c93605dd
vogeaux <kevin.hamon@campus-igs-toulous.fr>
2022-03-21 22:54:26 +0100

    1er

--- rename.py
+++
@@ -1,9 +0,0 @@
-import os
-
-for path, subdirs, files in os.walk('./test'):
-    for name in files:
-        print(os.path.join(path, name))
-        filepath = os.path.join(path, name)
-
-        filepath2=filepath.replace('.zooky','')
-        os.rename(filepath, filepath2)
